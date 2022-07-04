bbcdcaba42c9f64b5167ec284a3287b72c0e1b62
MirzaAyon <ayonmirza98@gmail.com>
2022-07-04 12:33:32 +0600

    math added

--- javascript simple coding problems/math.js
+++ javascript simple coding problems/math.js
@@ -1,5 +1,25 @@
 //abs
-const myNumber = -5;
-const output = Math.abs(myNumber);
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
 //ei line e mynumber bhul korle ref error dibe
-console.log(output);
+// console.log(output);
+
+const myNumber = 21.95;
+// const output = Math.ceil(myNumber); //uporer purno shongkhae utha
+// const output = Math.floor(myNumber); //nicher purno shongkhae nama
+// const output = Math.round(myNumber); //round kore .5 er upr hole upore utha r niche hole niche nama
+// console.log(output);
+
+//random
+// const output = Math.random();
+// console.log(output); //prottek bar run korle random num show korbe //1 theke 0 er modhe dibe
+const output = Math.random() * 6;
+//1 theke 6 er modhe random number dibe
+// console.log(output);
+const rounded = Math.round(output);
+const rounded2 = Math.floor(output); //6 er kom hbe 
+const rounded3 = Math.ceil(output); //6 er kom hbe 
+console.log(rounded);
+console.log(rounded2);
+console.log(rounded3);
+//1 - 6 er modhe round figure dibe
