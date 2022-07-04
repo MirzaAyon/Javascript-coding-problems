57c735936677e65229fdd8d184bc95e2b742c04c
MirzaAyon <ayonmirza98@gmail.com>
2022-07-04 16:15:02 +0600

    max added

---
+++ javascript simple coding problems/max3.js
@@ -0,0 +1,6 @@
+const business = 1650;
+const minister = 850;
+const army = 900;
+
+var max = Math.max(business, minister, army);
+console.log('largest is', max);
---
+++ javascript simple coding problems/max4.js
@@ -0,0 +1,21 @@
+// compare 3
+// const business = 1650;
+// const minister = 850;
+// const army = 900;
+
+
+function findLargest(first, second) {
+    if (first > second) {
+        return first;
+    }
+    else {
+        return second;
+    }
+}
+
+const largest = findLargest(154, 241);
+console.log('largest is', largest)
+
+//task 1 : create a function that takes three numbers as input pm and returns you the largest number of the three
+
+//task 2 : write a function to find the smallest of three numbers 
