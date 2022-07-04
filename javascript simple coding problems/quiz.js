f12ae2993d4e9cca4092cc5364733e6b021eaf86
MirzaAyon <ayonmirza98@gmail.com>
2022-07-04 22:50:14 +0600

    some problems added

---
+++ javascript simple coding problems/quiz.js
@@ -0,0 +1,35 @@
+// 3
+// Find the largest?
+
+// var a = 5; var b = 8; var c = 4; var d = 6; var highest =___?___(a,b, c, d);
+//ans : Math.max
+
+//11
+// function add(a, b){
+//     return a + b;
+//   }
+//   console.log(add("adam" + "eve"))
+// ans: adameveundefined
+
+//14
+// Write a code to generate a random number between 100 to 200:
+
+// var result = 100 + _______?_______;
+
+// ans: Math.random()*100;
+
+
+// 15
+// Q15: What code you have to write to know the number of “a” in a string
+
+// var sentence = "I am hardworking. I am determined. I will be a web developer.";
+// var count = 0;
+// for(var i = 0; i < sentence.length; i++){
+//   var letter = sentence[i];
+//   if(________?______){
+//     count++;
+//   }
+// }
+// console.log(count)
+
+//ans : letter == "a";
