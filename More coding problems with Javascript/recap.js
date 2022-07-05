d014d80aecef3ec5fbdb111901026b484cefa113
MirzaAyon <ayonmirza98@gmail.com>
2022-07-05 11:00:54 +0600

    max number , min number added

---
+++ More coding problems with Javascript/recap.js
@@ -0,0 +1,24 @@
+//max number from the array
+function largestElement(numbers) {
+    let max = numbers[0]; // 
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element > max) {
+            max = element;
+        }
+    }
+}
+
+
+//max number from the array
+
+function largestElement(numbers) {
+    let min = numbers[0]; // 
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element < min) {
+            min = element;
+        }
+    }
+}
+
