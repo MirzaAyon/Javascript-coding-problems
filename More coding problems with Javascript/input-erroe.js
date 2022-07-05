c71298733dd598e9ea0d9cce8930e5298abb4b28
MirzaAyon <ayonmirza98@gmail.com>
2022-07-05 19:57:04 +0600

    input-error added

---
+++ More coding problems with Javascript/input-erroe.js
@@ -0,0 +1,18 @@
+// function add(a, b) {
+//     const c = a + b;
+//     return c;
+// }
+
+// ans = add(2 + 3);
+// console.log(ans); 
+//nan dibe
+
+
+function add(a, b) {
+    console.log('parameters', a, b);
+    const c = a + b;
+    return c;
+}
+
+ans = add(2 + 3);
+console.log(ans); 
