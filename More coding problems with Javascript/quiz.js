ec1237b5c92aa3991573b599ff6132d2357c5fce
MirzaAyon <ayonmirza98@gmail.com>
2022-07-06 00:59:54 +0600

    quiz added

---
+++ More coding problems with Javascript/quiz.js
@@ -0,0 +1,19 @@
+// 1
+//Is JavaScript case sensitive? ans : yes
+
+// 2
+// Which one is false?
+//you can write only one return inside a function
+
+// 7
+// let sum=0; for( let i = 0; i<=3;i++){ sum = sum + i;
+//6 ans
+
+//How will you delete the age property of the object student (google it)
+
+// var student =  { name : "Yo Mama", age : 17 };
+//delete student.age;
+
+// 10
+// How will you change the last element of an array? (google it)
+//arr[arr.length - 1] = 5;
