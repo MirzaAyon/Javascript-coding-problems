daec3878f11d7e5723ae871d92eb20253b7dd384
MirzaAyon <ayonmirza98@gmail.com>
2022-07-06 00:18:55 +0600

    total-price added of a cart

---
+++ More coding problems with Javascript/total-price.js
@@ -0,0 +1,13 @@
+const products = [
+    { name: 'laptop', price: 43000 },
+    { name: 'shirt', price: 500 },
+    { name: 'watch', price: 3680 },
+    { name: 'phone', price: 55000 }
+];
+
+let totalPrice = 0;
+
+for (const product of products) {
+    totalPrice = totalPrice + product.price;
+}
+console.log(totalPrice);
