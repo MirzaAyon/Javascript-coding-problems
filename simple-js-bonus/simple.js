b7f7394b7af9aa5eb5f23437f514c126c54ba0ae
MirzaAyon <ayonmirza98@gmail.com>
2022-07-06 12:29:10 +0600

    simple js loop with function added

--- simple-js-bonus/simple.js
+++ simple-js-bonus/simple.js
@@ -1,3 +1,37 @@
-for (let i = 0; i < 6; i++) {
+// for (let i = 0; i < 6; i++) {
+//     console.log(i); //ekhane breakpoint dibo
+// }
+
+
+//function
+// function consoleNumber(i) {
+//     console.log(i);
+// }
+// consoleNumber(1);
+
+
+//bar bar korar jnno
+// function consoleNumber(i) {
+//     console.log(i);
+//     consoleNumber(i); //infinity loop e cholte thakbe ekta time por theme jabe 
+// }
+// consoleNumber(1);
+
+//diff between i++ and ++i google krbo
+
+// function consoleNumber(i) {
+//     console.log(i);
+//     consoleNumber(i + 1);
+// }
+// consoleNumber(1); //ekhn ek ek kore barbe 
+
+
+function consoleNumber(i) {
+    if (i > 5) { //ekhane breakpoint hbe 
+        return;
+    }
     console.log(i);
+    consoleNumber(i + 1);
 }
+consoleNumber(1);
+
